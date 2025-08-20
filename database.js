b07a4fd1edb403b999156da51fa46a0c67ac6f58
Shantoatiq <149778777+Shantoatiq@users.noreply.github.com>
2025-08-20 14:12:32 +0600

    Create database.js

---
+++ database.js
@@ -0,0 +1,8 @@
+// Simulated Database
+let database = {
+  products: [
+    { name: "Pizza", price: 10, img: "https://source.unsplash.com/200x200/?pizza" },
+    { name: "Burger", price: 8, img: "https://source.unsplash.com/200x200/?burger" }
+  ],
+  orders: []
+};
